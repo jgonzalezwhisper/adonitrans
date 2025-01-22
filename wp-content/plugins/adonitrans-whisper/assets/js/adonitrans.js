3985dc1249a56287ae745eafc471a6f8c7ee3f78
belial9826Hot <julian9826@hotmail.com>
2025-01-21 21:36:18 -0500

    archivos compilados

--- wp-content/plugins/adonitrans-whisper/assets/js/adonitrans.js
+++ wp-content/plugins/adonitrans-whisper/assets/js/adonitrans.js
@@ -1,2 +1,2 @@
-jQuery(document).ready(function(e){e(document).on("click",".adonitrans-plug .formplug label i",function(t){t.preventDefault(),$ojo=e(this);t=e(this).closest("label").find("input");"password"===t.attr("type")?t.attr("type","text"):t.attr("type","password"),e(this).toggleClass("icofont-eye icofont-eye-blocked")})});
+jQuery(document).ready(function(e){e(document).on("click",".adonitrans-plug .formplug label.password i",function(t){t.preventDefault(),$ojo=e(this);t=e(this).closest("label").find("input");"password"===t.attr("type")?t.attr("type","text"):t.attr("type","password"),e(this).toggleClass("icofont-eye icofont-eye-blocked")})});
 //# sourceMappingURL=adonitrans.js.map
