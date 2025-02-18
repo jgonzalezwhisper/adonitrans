2168f40a09672fcc3faf0a5867b0c0e7513be13a
belial9826Hot <julian9826@hotmail.com>
2025-02-17 20:00:31 -0500

    mejoras de codigo panel conductor y campos acf

--- wp-content/plugins/adonitrans-whisper/includes/ajax/ajaxconductor.php
+++ wp-content/plugins/adonitrans-whisper/includes/ajax/ajaxconductor.php
@@ -1,7 +1,7 @@
 <?php 
+date_default_timezone_set('America/Bogota');
 add_action('wp_ajax_guardar_peajes', 'func_guardar_peajes');
 add_action('wp_ajax_nopriv_guardar_peajes', 'func_guardar_peajes');
-
 function func_guardar_peajes() {
     if (empty($_POST['post_id'])) {
         wp_send_json_error('Datos incompletos');
@@ -59,4 +59,185 @@ function func_guardar_peajes() {
     update_field('peajes_del_recorrido', $peajes, $post_id);
 
     wp_send_json_success('Peajes guardados correctamente');
+}
+
+add_action('wp_ajax_finalizar_recorrido_conductor', 'func_finalizar_recorrido_conductor');
+add_action('wp_ajax_nopriv_finalizar_recorrido_conductor', 'func_finalizar_recorrido_conductor');
+function func_finalizar_recorrido_conductor() {
+    if (empty($_POST['post_id'])) {
+        wp_send_json_error('Datos incompletos');
+        return;
+    }
+
+    $post_id = intval($_POST['post_id']);
+
+    $contpeajes     = 0; /*Peajes*/
+    $contrecogidos  = 0; /*Usuarios adicionales recogidos*/
+    $trayecto_commen = isset($_POST['trayecto_commen']) ? $_POST['trayecto_commen'] : "";
+
+    /*DATOS DE PEAJES*/
+    $nombres = $_POST['nomb_peaje'];
+    $valores = $_POST['valor_peaje'];
+    $comprobantes = $_FILES['comprobante_pago_peaje'];
+
+    if (!empty($_POST['valor_peaje'])) {
+        $contpeajes = count($_POST['valor_peaje']);
+    }
+    
+    $peajes = [];
+
+    foreach ($nombres as $index => $nombre) {
+        $valor = $valores[$index];
+        $comprobante_id = '';
+
+        if (!empty($comprobantes['name'][$index])) {
+            $file = [
+                'name'     => $comprobantes['name'][$index],
+                'type'     => $comprobantes['type'][$index],
+                'tmp_name' => $comprobantes['tmp_name'][$index],
+                'error'    => $comprobantes['error'][$index],
+                'size'     => $comprobantes['size'][$index],
+            ];
+
+            $upload = wp_handle_upload($file, ['test_form' => false]);
+
+            if (!isset($upload['error'])) {
+                $attachment = [
+                    'post_mime_type' => $upload['type'],
+                    'post_title'     => sanitize_file_name($file['name']),
+                    'post_content'   => '',
+                    'post_status'    => 'inherit',
+                ];
+                $attach_id = wp_insert_attachment($attachment, $upload['file']);
+
+                require_once ABSPATH . 'wp-admin/includes/image.php';
+                $attach_data = wp_generate_attachment_metadata($attach_id, $upload['file']);
+                wp_update_attachment_metadata($attach_id, $attach_data);
+
+                $comprobante_id = $attach_id; // Guarda el ID en lugar de la URL
+            }
+        }
+
+        if (!empty($nombre) && !empty($valor)) {
+            $peajes[] = [
+                'nombre'      => sanitize_text_field($nombre),
+                'valor'       => sanitize_text_field($valor),
+                'comprobante' => $comprobante_id, // Ahora guarda el ID de la imagen
+            ];
+        }
+    }
+    update_field('peajes_del_recorrido', $peajes, $post_id);
+
+    /*USUARIOS ADICIONALES*/
+    if (isset($_POST['usuarios_adicionales']) && !empty($_POST['usuarios_adicionales'])) {
+
+        $usuarios_adicionales = array_filter($_POST['usuarios_adicionales']);
+        $contrecogidos = count($usuarios_adicionales);
+
+        update_field('pasajeros_recogidos_recorrido', $contrecogidos, $post_id);
+    }
+
+    /*DATOS DE TIEMPO*/
+    update_field('hora_llegada_recorrido', $_POST['horaLlegada'], $post_id);
+    update_field('hora_inicio_recorrido', $_POST['horaInicio'], $post_id);
+    update_field('hora_final_recorrido', $_POST['horaFin'], $post_id);
+    update_field('tiempo_de_espera_recorrido', $_POST['minutosExtras'], $post_id);    
+
+    /*TOMAR VALORES PARA ENVIAR LOS TOTALES*/
+    $args_tarifa = array(
+        'post_type'      => 'tarifa',
+        'meta_query'     => array(
+            'relation' => 'AND',
+            array(
+                'key'   => 'empresa_aplicar_tarifa',
+                'value' => get_field('empresa_solicitante_recorrido',$post_id)->ID,
+            ),
+            array(
+                'key'   => 'ano_aplicar_tarifa',
+                'value' => date('Y'),
+            ),
+        ),
+        'fields'         => 'ids',
+        'posts_per_page' => 1,
+    );
+
+    $tarifas = get_posts($args_tarifa);
+    $res_tarifa = [];
+
+    if (empty($tarifas)) {
+        wp_send_json_error('No hay tarifas configuradas para la empresa.');
+        return;
+    }
+
+    if (!empty($tarifas)) {
+        $tarifa_id = $tarifas[0];
+        $tarifas_base_empresa = get_field('tarifas_base_empresa', $tarifa_id);
+
+        /*error_log("ACF TARIFAS BASE EMPRESA ".print_r($tarifas_base_empresa,true));*/
+
+        if ($tarifas_base_empresa) {            
+
+            foreach ($tarifas_base_empresa as $tarifa_base) {
+                $res_tarifa[] = [
+                    'codigo' => $tarifa_base['codigo'],
+                    'nombre' => $tarifa_base['nombre'],
+                    'valor'  => $tarifa_base['valor'],
+                ];
+            }
+        }
+    }    
+
+    /*Haciendo calculos*/
+    $total_recorrido = 0;
+    $costos = [
+        ['motivo' => 'Recorrido Inicial', 'valor' => intval(get_field('valor_ruta_recorrido',$post_id)) ],
+    ];
+
+    $total_recorrido += intval(get_field('valor_ruta_recorrido',$post_id));
+
+    foreach ($res_tarifa as $key => $value) {
+
+        $nombre_tarifa_normalizado = strtolower($value['nombre']);
+
+        if (strpos($nombre_tarifa_normalizado, 'pasajero adicional') !== false) {
+            $costos[] = ['motivo' => 'Usuario(s) Adicional(es)', 'valor' => intval($contrecogidos * $value['valor']) ];
+            $total_recorrido += intval($contrecogidos * $value['valor']);
+        }
+        if (strpos($nombre_tarifa_normalizado, 'peajes') !== false && !empty($_POST['valor_peaje'])) {
+            $total_peajes = array_sum($valores);
+            $nume_peajes = $contpeajes * $value['valor'];
+            $costos[] = ['motivo' => 'Peajes', 'valor' => intval($total_peajes + $nume_peajes) ];
+            $total_recorrido += intval($total_peajes + $nume_peajes);
+        }
+        if (strpos($nombre_tarifa_normalizado, 'tiempo espera') !== false) {
+            $costos[] = ['motivo' => 'Tiempo de Espera', 'valor' => intval($_POST['minutosExtras'] * $value['valor']) ];
+            $total_recorrido += intval($_POST['minutosExtras'] * $value['valor']);
+        }
+    }
+
+    $costos[] = ['motivo' => 'Total Recorrido', 'valor' => intval($total_recorrido) ];
+
+    $arr_costos_calculados = [];
+
+    // Recorrer los datos y agregarlos al array
+    foreach ($costos as $dato) {
+        $arr_costos_calculados[] = [
+            'motivo' => $dato['motivo'],
+            'valor' => $dato['valor']
+        ];
+    }
+
+    update_field('costo_calculado_del_recorrido', $arr_costos_calculados, $post_id);
+    update_field('comentario_conductor_final_recorrido', $trayecto_commen, $post_id);
+
+    /*CAMBIO DE ESTADO*/
+    update_field('estado_del_recorrido', 'Finalizado', $post_id);
+
+
+    $response = array(
+        'message' => 'Recorrido Finalizado Correctamente',
+        'datos'    => $costos
+    );
+
+    wp_send_json_success($response);
 }
--- wp-content/plugins/adonitrans-whisper/includes/parts/panel/conductor.php
+++ wp-content/plugins/adonitrans-whisper/includes/parts/panel/conductor.php
@@ -44,7 +44,7 @@
                 <input type="hidden" name="post_id" value="<?= $post_id ?>">
                 <input type="hidden" id="tiempo_espera" value="<?= $tiempo_de_espera_para_recorrido ?>">
                 <div id="wrap-contador-espera" class="ocultar">
-                    <div class="contador">00:00</div>
+                    <div class="contador">Tiempo de espera total 00:00</div>
                     <a href="#" class="btn button" data-action="iniciar" disabled>Inicio de Ruta</a>
                     <input type="hidden" name="tiempo_de_espera" id="tiempo_de_espera" value="">
                 </div>
@@ -80,12 +80,7 @@
                     </div>
                     <div class="column column-3">
                         <div class="wrap trayecto_acciones">
-                            <div class="trayecto_bottons binicio">
-                                <a href="#" class="btn button" data-action="llegada">Llegada al Destino</a>                               
-                                <a href="#" class="btn button" data-action="cancelar">Cancelar Ruta</a>
-                                <a href="#" class="btn button save-info ocultar" data-action="save-info">Guardar Informacion</a>
-                                <a href="#" class="btn button end-recorrido ocultar" data-action="end-recorrido">Finalizar Recorrido</a>
-                            </div>
+
                             <div id="wrap-listadocheck" class="trayecto_bottons bmedio ocultar">
                                 <h5><strong>Seguimiento de Tiempo</strong></h5>
                                 <ul id="list-info-recorrido">
@@ -95,6 +90,12 @@
                                     <li><strong>Parada 2:</strong> <span>9:10 a.m</span></li>
                                     <li><strong>Finalizacion Recorrido:</strong> <span>9:10 a.m</span></li> -->
                                 </ul>
+                            </div>
+                            <div class="trayecto_bottons binicio">
+                                <a href="#" class="btn button" data-action="llegada">Llegada al Destino</a>                               
+                                <a href="#" class="btn button" data-action="cancelar">Cancelar Ruta</a>
+                                <a href="#" class="btn button save-info ocultar" data-action="save-info">Guardar Informacion</a>
+                                <a href="#" class="btn button end-recorrido ocultar" data-action="end-recorrido">Finalizar Recorrido</a>
                             </div>
                         </div>
                     </div>
@@ -115,16 +116,16 @@
 
                             // Aquí puedes obtener el nombre y apellido del usuario basado en el $id_usuario_adicional
                             // Esto depende de cómo tengas almacenados los usuarios en tu sistema
-                            $nombre_usuario = get_the_author_meta('display_name', $id_usuario_adicional);
+                            $nombre_usuario = get_the_author_meta('first_name', $id_usuario_adicional);
                             $apellido_usuario = get_the_author_meta('last_name', $id_usuario_adicional);
 
                             // Generar el HTML para cada fila
                             ?>
                             <div class="usuario-adicional">
                                 <input type="checkbox" name="usuarios_adicionales[]" value="<?php echo esc_attr($id_usuario_adicional); ?>">
-                                <span class="nombre-usuario"><?php echo esc_html($nombre_usuario . ' ' . $apellido_usuario); ?></span>
-                                <span class="origen"><?php echo esc_html($origen . ' (' . $direccion_origen . ')'); ?></span>
-                                <span class="destino"><?php echo esc_html($destino . ' (' . $direccion_destino . ')'); ?></span>
+                                <span class="nombre-usuario"><?php echo esc_html($nombre_usuario . ' ' . $apellido_usuario); ?> </span>
+                                <span class="origen"> -- <?php echo esc_html($origen . ' (' . $direccion_origen . ')'); ?></span>
+                                <span class="destino"> - <?php echo esc_html($destino . ' (' . $direccion_destino . ')'); ?></span>
                             </div>
                             <?php
                         endforeach;
@@ -134,7 +135,7 @@
                 endif;
                 ?>
 
-                <div class="wrap trayecto_finalizar">
+                <div id="wrap_trayecto_finalizar" class="wrap trayecto_finalizar ocultar">
                     <h3 class="titulo">Finaliza el Servicio</h3>
 
                     <div id="wrap-peajes" class="wrap wrap-fanjas">
@@ -170,17 +171,17 @@
                         <label>Comentarios adicionales:</label>
                         <textarea id="trayecto_commen" name="trayecto_commen" rows="8" cols="115"></textarea>
                     </div>
-                    <div class="trayecto_total">                        
+                    <div id="wrap-total-calculo" class="trayecto_total ocultar">                        
                         <div class="trayecto_total_detail">
                             <ul>
-                                <li><strong>Recorrido Inicial:</strong> <span class="symbol">$<span class="price">35.000</span></span></li>
+                                <!-- <li><strong>Recorrido Inicial:</strong> <span class="symbol">$<span class="price">35.000</span></span></li>
                                 <li><strong>Trayectos Adicionales:</strong> <span class="symbol">$<span class="price">55.000</span></span></li>
                                 <li><strong>Peajes:</strong> <span class="symbol">$<span class="price">55.000</span></span></li>
                                 <li><strong>Usuario(s) Adicional(es):</strong> <span class="symbol">$<span class="price">75.000</span></span></li>
-                                <li><strong>Tiempo de Espera:</strong> <span class="symbol">$<span class="price">15.000</span></span></li>
+                                <li><strong>Tiempo de Espera:</strong> <span class="symbol">$<span class="price">15.000</span></span></li> -->
                             </ul>
                         </div>
-                        <h4>Total servicio: <span>$51.000</span></h4>
+                        <h4>Total servicio: <span></span></h4>
                     </div>
                 </div>   
             </form>
